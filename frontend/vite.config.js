a3788650c0f22d51d2ad31d490138e19d57dfb5f
Dhiraj Mishra <work.dhirajmishra@gmail.com>
2025-03-28 16:39:44 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://127.0.0.1:5000", // Use 127.0.0.1 instead of localhost
+      "/api": "https://clothconnect-backend.onrender.com", // 
     },
   },
 });
